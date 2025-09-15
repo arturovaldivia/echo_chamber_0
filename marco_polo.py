ee76e5615f6924b4cc04711aea3a1cd6aa89ace8
arturovaldivia <arturovaldivia@users.noreply.github.com>
2025-09-15 19:57:32 +0200

    Update marco_polo.py

--- marco_polo.py
+++ marco_polo.py
@@ -29,4 +29,4 @@ def home():
     return "Text processing API is running 🚀"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
